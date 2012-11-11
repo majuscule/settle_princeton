2b04caca87e291a0306d7f4593d9eb2e5af305a2
Dylan Lloyd <dylan@dylansserver.com>
2012-11-11 06:45:51 +0000

    db schema added, docs now added on form submit

--- run.py
+++ run.py
@@ -1,14 +1,35 @@
 from flask import Flask, render_template, request
+from mongokit import Connection, Document
 
 app = Flask(__name__)
- 
+
+class Player(Document):
+    structure = {
+        'name': unicode,
+        'phone': unicode,
+    }
+    validators = {
+        'name': lambda x: True,
+        'phone': lambda x: True
+    }
+    required_fields = ['name', 'phone']
+    use_dot_notation = True
+
+connection = Connection()
+connection.register([Player])
+collection = connection.games.players
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def signup():
-    return request.form['name'] + ':' + request.form['phone']
+    player = collection.Player()
+    player['name'] = request.form['name']
+    player['phone'] = request.form['phone']
+    player.save()
+    return request.form['name'] + ':' + request.form['phone'] + '<br>SAVED'
  
 if __name__ == '__main__':
     app.run(debug=True)
