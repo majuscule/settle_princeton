f47f4f8471e521b2fc392614ffacd4813ca4103c
Dylan Lloyd <dylan@dylansserver.com>
2012-11-11 10:06:26 +0000

    some changes

--- run.py
+++ run.py
@@ -19,6 +19,7 @@ connection = Connection()
 connection.register([Player])
 triplets = connection.games.triplets
 quads = connection.games.quads 
+singles = connection.games.singles
 
 @app.route('/', methods=['GET'])
 def index():
@@ -26,26 +27,48 @@ def index():
 
 @app.route('/', methods=['POST'])
 def signup():
-    def add(collection):
+    ready = False
+    def addTo(collection):
         player = collection.Player()
         player['name'] = request.form['name']
         player['phone'] = request.form['phone']
         player.save()
-        if collection.count() == 3:
-            connection.games.drop_collection('players')
-            return "<h1>PLAY NOW</h1>"
-    if request.form['size'] == 3:
-        add(triplets)
-    elif request.form['size'] == 4:
-        add(quads)
-    elif request.form['size'] == 0:
-        return "<h1>We'll let you know</h1>"
-    return request.form['name'] + ':' + request.form['phone'] + '-' + request.form['size'] + '-' + request.form['updates'] + '<br>SAVED'
+    def checkReady(collection, i):
+        count = collection.count()
+        ready = False
+        if count == i:
+            ready = True
+        elif count == i-1 and singles.count() > 0:
+            #pop off single
+            ready = True
+        if ready:
+            collection.remove()
+        return ready
+    if request.form['size'] == '3':
+        addTo(triplets)
+        ready = checkReady(triplets, 3)
+    elif request.form['size'] == '4':
+        addTo(quads)
+        ready = checkReady(quads, 4)
+    elif request.form['size'] == '0':
+        addTo(singles)
+        ready == checkReady(quads, 4) or checkReady(triplets, 3)
+    if ready:
+        return "play!"
+    else:
+        return "we'll let you know"
+    #return request.form['name'] + ':' + request.form['phone'] + '-' + request.form['size'] + '-' + request.form['updates']
 
 @app.route('/players')
 def list():
-    string = str(collection.count()) + ' Players:<br><br>'
-    for player in collection.Player.find():
+    string = str(quads.count()) + ' Quad Players:<br>'
+    for player in quads.Player.find():
+         string += player.name + ':' + player.phone + '<br>'
+    string += str(triplets.count()) + ' Triplet Players:<br>'
+    for player in triplets.Player.find():
+         string += player.name + ':' + player.phone + '<br>'
+    string += str(singles.count()) + ' Single Players:<br>'
+    for player in singles.Player.find():
          string += player.name + ':' + player.phone + '<br>'
     return string
  
