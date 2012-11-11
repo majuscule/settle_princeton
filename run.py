6be17e9b4295b82de45a9572e3a785af10410a79
Dylan Lloyd <dylan@dylansserver.com>
2012-11-11 06:08:32 +0000

    remove useless place

--- run.py
+++ run.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
  
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/signup', methods=['POST'])
+@app.route('/', methods=['POST'])
 def signup():
     return request.form['name'] + ':' + request.form['phone']
  
